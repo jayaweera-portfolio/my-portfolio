b303db64fb7c4cc8aeb953b518ae6f8a00ee04fd
Sachithra Jayaweera <sachithrajayaweera4u@gmail.com>
2023-09-02 22:54:30 +0530

    card changes

--- components/servises_section/my_services.tsx
+++ components/servises_section/my_services.tsx
@@ -7,6 +7,72 @@ const windowWidth = Dimensions.get('window').width;
 
 const isMobile = windowWidth < 768;
 
+const styles = StyleSheet.create({
+
+    cardWithBorder: {
+        borderColor: 'black', // Add border color
+        borderWidth: 1, // Add border width
+    },
+    container: {
+        marginTop: 16,
+        padding: 16,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 16,
+        marginLeft: 100,
+        marginRight:100,
+    },
+    column: {
+        width: isMobile ? '100%' : '30%',
+        marginBottom: isMobile ? 16 : 0,
+        marginRight: isMobile ? 0 : 16,
+        marginLeft: isMobile ? 0 : 16,
+        flex: 1,
+        //marginRight: 8,
+        padding: 20,
+        //width: '20%'
+    },
+    card: {
+        height: 280,
+        borderWidth: 1,
+        borderColor: 'transparent',
+        borderRadius: 8,
+        padding: 50,
+        justifyContent: 'center',
+    },
+    servicesText: {
+        // Removed cursor property
+    },
+    cardBody: {
+        alignItems: 'center',
+    },
+    servicesIcon: {
+        fontSize: 36,
+        textAlign: 'center',
+        width: '100%',
+    },
+    cardTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 12,
+    },
+    cardText: {
+        fontSize: 16,
+        textAlign: 'center',
+        marginTop: 8,
+    },
+
+});
+
 const MyServices = () => {
 
     return (
@@ -19,7 +85,7 @@ const MyServices = () => {
                         <TouchableHighlight
                             style={[styles.card, styles.servicesText, styles.cardWithBorder]}
                             activeOpacity={0.7}
-                            underlayColor="#008000" // Highlight color on touch 
+                            underlayColor="#00C000" // Highlight color on touch 
                         >
                             <View style={styles.cardBody}>
                                 <FontAwesomeIcon icon={faClock} size="1x" style={styles.servicesIcon} />
@@ -39,7 +105,7 @@ const MyServices = () => {
                         >
                             <View style={styles.cardBody}>
                                 <FontAwesomeIcon icon={faClock} size="1x" style={styles.servicesIcon} />
-                                <Text style={styles.cardTitle}>Website Development</Text>
+                                <Text style={styles.cardTitle}>Website Design</Text>
                                 <Text style={styles.cardText}>
                                     Some quick example text to build on the card title and make up the bulk of the card's content.
                                 </Text>
@@ -56,7 +122,7 @@ const MyServices = () => {
                         >
                             <View style={styles.cardBody}>
                                 <FontAwesomeIcon icon={faClock} size="1x" style={styles.servicesIcon} />
-                                <Text style={styles.cardTitle}>Website Development</Text>
+                                <Text style={styles.cardTitle}>Website Deployment</Text>
                                 <Text style={styles.cardText}>
                                     Some quick example text to build on the card title and make up the bulk of the card's content.
                                 </Text>
@@ -72,7 +138,7 @@ const MyServices = () => {
                         >
                             <View style={styles.cardBody}>
                                 <FontAwesomeIcon icon={faClock} size="1x" style={styles.servicesIcon} />
-                                <Text style={styles.cardTitle}>Website Development</Text>
+                                <Text style={styles.cardTitle}>SEO</Text>
                                 <Text style={styles.cardText}>
                                     Some quick example text to build on the card title and make up the bulk of the card's content.
                                 </Text>
@@ -88,7 +154,7 @@ const MyServices = () => {
                         >
                             <View style={styles.cardBody}>
                                 <FontAwesomeIcon icon={faClock} size="1x" style={styles.servicesIcon} />
-                                <Text style={styles.cardTitle}>Website Development</Text>
+                                <Text style={styles.cardTitle}>DevOps</Text>
                                 <Text style={styles.cardText}>
                                     Some quick example text to build on the card title and make up the bulk of the card's content.
                                 </Text>
@@ -104,7 +170,7 @@ const MyServices = () => {
                         >
                             <View style={styles.cardBody}>
                                 <FontAwesomeIcon icon={faClock} size="1x" style={styles.servicesIcon} />
-                                <Text style={styles.cardTitle}>Website Development</Text>
+                                <Text style={styles.cardTitle}>QA</Text>
                                 <Text style={styles.cardText}>
                                     Some quick example text to build on the card title and make up the bulk of the card's content.
                                 </Text>
@@ -230,76 +296,5 @@ const MyServices = () => {
     );
 };
 
-
-const styles = StyleSheet.create({
-
-    cardWithBorder: {
-        borderColor: 'black', // Add border color
-        borderWidth: 1, // Add border width
-    },
-    container: {
-        marginTop: 16,
-        padding: 16,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: 16,
-        marginLeft: 100,
-        marginRight:100,
-    },
-    column: {
-        width: isMobile ? '100%' : '30%',
-        marginBottom: isMobile ? 16 : 0,
-        marginRight: isMobile ? 0 : 16,
-        marginLeft: isMobile ? 0 : 16,
-        flex: 1,
-        //marginRight: 8,
-        padding: 20,
-        //width: '20%'
-    },
-    card: {
-        height: 280,
-        borderWidth: 1,
-        borderColor: 'transparent',
-        borderRadius: 8,
-        padding: 50,
-        justifyContent: 'center',
-    },
-    servicesText: {
-        // Removed cursor property
-    },
-    cardBody: {
-        alignItems: 'center',
-    },
-    servicesIcon: {
-        fontSize: 36,
-        textAlign: 'center',
-        width: '100%',
-    },
-    cardTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginTop: 12,
-    },
-    cardText: {
-        fontSize: 16,
-        textAlign: 'center',
-        marginTop: 8,
-    },
-    cardHover: {
-        borderColor: '#008000',
-    },
-    servicesIconHover: {
-        color: '#008000',
-    },
-});
 
 export default MyServices;
